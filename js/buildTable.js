e0c0872d8050a14623b89577ff9c3620ce62e60b
shtora123 <shtora123@local>
2026-05-02 22:16:34 +0300

    build table

--- js/buildTable.js
+++ js/buildTable.js
@@ -1,29 +1,78 @@
+
+
+async function loadData() {
+    const response = await fetch('./score_table.json');
+    const data = await response.json();
+    return data;
+}
+
+async function buildScores(){
+    const scoreTable = await loadData();
+
+   
+    buildBox ([[7, 8, 9, 10, 12], [5, 5, 6, 7, 8, ], [3, 3, 4, 5, 6, ], [2, 2, 3, 3, 4, ]], document.body)
+
+}
+  
+  
+
 function buildTables (scoreTable) {
-    const womenNoSmokers = scoreTable.filter(record => record.gender === 'female' && record.isSomker === flase)
-    const womenSmokers = scoreTable.filter(record => record.gender === 'female' && record.isSomker === true)
-    const menNoSmokers = scoreTable.filter(record => record.gender === 'male' && record.isSomker === flase)
-    const menSmokers = scoreTable.filter(record => record.gender === 'male' && record.isSomker === true)
+    const womenNoSmokers = scoreTable.filter(record => record.gender === 'female' && record.isSmoker === false)
+    const womenSmokers = scoreTable.filter(record => record.gender === 'female' && record.isSmoker === true)
+    const menNoSmokers = scoreTable.filter(record => record.gender === 'male' && record.isSmoker === false)
+    const menSmokers = scoreTable.filter(record => record.gender === 'male' && record.isSmoker === true)
 
     buildTable(womenNoSmokers)
     buildTable(womenSmokers)
     buildTable(menNoSmokers)
     buildTable(menSmokers)
 }
+
+
+
 function buildTable (scoreTable) { 
-    
+    for(var i = 0; i < scoreTable.length; i++){
+        if(i % 5 != 0){
+            drawCell(scoreTable[i].score, document.body)
+        } else {
+
+        }
+
+    }
+}
+
+function buildBox (scoreBox, container) {
+    const scoreBoxElement = document.createElement("div");
+    container.appendChild(scoreBoxElement)
+    scoreBoxElement.classList.add("score-box")
+
+    for(var i = 0; i < scoreBox.length; i ++){
+
+        buildLine(scoreBox[i], scoreBoxElement)
+    }
+}
+
+function buildLine (scoreRow, container){
+    const scoreRowElement = document.createElement("div");
+    container.appendChild(scoreRowElement)
+    scoreRowElement.classList.add("score-row")
+
+    for(var i = 0; i < scoreRow.length; i ++){
+
+        drawCell(scoreRow[i], scoreRowElement)
+    }
+
 }
 
 function drawCell (scoreNumber, container) {
     const scoreClass = getScoreCategory (scoreNumber)
     
     const cellElement = document.createElement("div");
-
-    cellElement.innerHTML = 
+    container.appendChild(cellElement)
+    cellElement.outerHTML = 
     `<div class="score-cell ${scoreClass}">
     ${scoreNumber}
-    <div>`
-
-    container.appendChild(cellElement)
+    </div>`
 }
 function getScoreCategory (score) { 
     if (score < 1){
@@ -46,13 +95,16 @@ function getScoreCategory (score) {
     }
     return 'danger-risk'
     
-    
 }
 
-drawCell(0, document.body)
-drawCell(1, document.body)
-drawCell(2, document.body)
-drawCell(3, document.body)
-drawCell(5, document.body)
-drawCell(10, document.body)
-drawCell(15, document.body)
+
+
+// drawCell(0, document.body)
+// drawCell(1, document.body)
+// drawCell(2, document.body)
+// drawCell(3, document.body)
+// drawCell(5, document.body)
+// drawCell(10, document.body)
+// drawCell(44, document.body)
+
+buildScores()
